a000e810d687bd0a7718b547c8d011c91c3a92e2
Beam <62375505+beamuu@users.noreply.github.com>
2025-11-19 21:40:05 +0700

    feat: apply Inter font across radix theme

---
+++ src/lib/themeConfig.ts
@@ -0,0 +1,8 @@
+import type { ThemeProps } from '@radix-ui/themes'
+
+export const radixThemeConfig: ThemeProps = {
+  accentColor: 'grass',
+  grayColor: 'sand',
+  radius: 'large',
+  scaling: '100%',
+}
